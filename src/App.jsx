215afe86ed533f63cd125c7907295fb03d73c194
ubeyid oumer <ubeyidah@gmail.com>
2024-04-16 19:30:12 +0300

    starting file

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <fieldset>
+        <legend>title</legend>
+        <input type="checkbox" />
+      </fieldset>
+    </div>
+  )
+}
